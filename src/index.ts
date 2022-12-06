20a594ab54b3885a728ebbffa634d2560a0faec7
Pedro Filho <pedro@filho.me>
2022-12-06 19:02:52 +0000

    adding @polkadot/api-augment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,59 @@
+import "@polkadot/api-augment";
+
 import { web3FromAddress } from "@polkadot/extension-dapp";
 import type { ISubmittableResult } from "@polkadot/types/types";
 import type { GenerateMultisigParams } from "./types";
+
+const getSignAndSendCallback = ({
+  onDropped,
+  onError,
+  onExecuted,
+  onInvalid,
+  onLoading,
+  onSuccess,
+}: Pick<
+  GenerateMultisigParams,
+  | "onDropped"
+  | "onError"
+  | "onExecuted"
+  | "onInvalid"
+  | "onLoading"
+  | "onSuccess"
+>) => {
+  return ({ events, status }: ISubmittableResult) => {
+    if (status.isInvalid) {
+      if (onInvalid) onInvalid();
+    } else if (status.isReady) {
+      if (onLoading) onLoading();
+    } else if (status.isDropped) {
+      if (onDropped) onDropped();
+    } else if (status.isInBlock || status.isFinalized) {
+      const multisigVoteStarted = events.find(
+        ({ event }) => event.method === "MultisigVoteStarted"
+      );
+
+      const multisigExecuted = events.find(
+        ({ event }) => event.method === "MultisigExecuted"
+      );
+
+      const failed = events.find(
+        ({ event }) => event.method === "ExtrinsicFailed"
+      );
+
+      if (multisigExecuted) {
+        if (onSuccess) onSuccess();
+      } else if (multisigVoteStarted) {
+        if (onSuccess) onSuccess();
+      } else if (failed) {
+        if (onError) onError();
+
+        console.error(failed.toHuman(true));
+      } else throw new Error("UNKNOWN_RESULT");
+    }
+
+    if (onExecuted) onExecuted();
+  };
+};
 
 const generateMultisig = async ({
   signer,
@@ -19,42 +72,6 @@ const generateMultisig = async ({
 }: GenerateMultisigParams): Promise<void> => {
   const injector = await web3FromAddress(signer);
 
-  const getSignAndSendCallback = () => {
-    return ({ events, status }: ISubmittableResult) => {
-      if (status.isInvalid) {
-        if (onInvalid) onInvalid();
-      } else if (status.isReady) {
-        if (onLoading) onLoading();
-      } else if (status.isDropped) {
-        if (onDropped) onDropped();
-      } else if (status.isInBlock || status.isFinalized) {
-        const multisigVoteStarted = events.find(
-          ({ event }) => event.method === "MultisigVoteStarted"
-        );
-
-        const multisigExecuted = events.find(
-          ({ event }) => event.method === "MultisigExecuted"
-        );
-
-        const failed = events.find(
-          ({ event }) => event.method === "ExtrinsicFailed"
-        );
-
-        if (multisigExecuted) {
-          if (onSuccess) onSuccess();
-        } else if (multisigVoteStarted) {
-          if (onSuccess) onSuccess();
-        } else if (failed) {
-          if (onError) onError();
-
-          console.error(failed.toHuman(true));
-        } else throw new Error("UNKNOWN_RESULT");
-      }
-
-      if (onExecuted) onExecuted();
-    };
-  };
-
   await api.tx.inv4
     .createIps(
       JSON.stringify(metadata),
@@ -65,5 +82,18 @@ const generateMultisig = async ({
       api.createType("OneOrPercent", { Percent: defaultAssetWeight }),
       defaultPermission
     )
-    .signAndSend(signer, { signer: injector.signer }, getSignAndSendCallback());
+    .signAndSend(
+      signer,
+      { signer: injector.signer },
+      getSignAndSendCallback({
+        onDropped,
+        onError,
+        onExecuted,
+        onInvalid,
+        onLoading,
+        onSuccess,
+      })
+    );
 };
+
+export { generateMultisig };
