03ae316c8b3d21356f2b19f0ce9de58aedc4af37
Pedro Filho <pedro@filho.me>
2023-01-25 16:47:39 +0000

    adding correct exports

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,4 @@ export * from "./utils";
 
 export * from "./multisig";
 
-export type {
-  CreateMultisigParams,
-  GetSignAndSendCallbackParams,
-  GetPendingMultisigCallsParams,
-  GetMultisigParams,
-  CreateMultisigCallParams,
-  VoteMultisigCallParams,
-  WithdrawVoteMultisigCallParams,
-} from "./types";
+export * from "./types";
