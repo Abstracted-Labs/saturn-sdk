23b6c09e9473b123495eac7b6ac76d75dbf7a80d
Pedro Filho <pedro@filho.me>
2023-05-23 11:40:14 +0200

    adding JSDoc to all public functions

--- src/multisig.ts
+++ src/multisig.ts
@@ -122,6 +122,12 @@ const setupTypes = ({
   return chains;
 };
 
+/**
+ * Saturn class represents a wrapper around the Polkadot API for interacting with multisig accounts and performing multisig operations.
+ *
+ * @class
+ * @public
+ */
 class Saturn {
   readonly api: ApiPromise;
   readonly chains: {
@@ -133,6 +139,13 @@ class Saturn {
   }[];
   feeAsset: FeeAsset;
 
+  /**
+   * Constructs a new instance of the Saturn class.
+   *
+   * @constructor
+   * @param {object} options - The options for initializing Saturn.
+   * @param {ApiPromise} options.api - The Polkadot API instance.
+   */
   constructor({ api }: { api: ApiPromise }) {
     if (!api.tx.inv4) {
       throw new Error("API_PROMISE_DOES_NOT_CONTAIN_INV4_MODULE");
@@ -147,14 +160,38 @@ class Saturn {
     this.feeAsset = FeeAsset.TNKR;
   }
 
+  /**
+   * Sets the fee asset for multisig operations.
+   *
+   * @public
+   * @param {FeeAsset} feeAsset - The fee asset to set.
+   * @returns {void}
+   */
   public setFeeAsset = (feeAsset: FeeAsset) => {
     this.feeAsset = feeAsset;
   };
 
+  /**
+   * Disconnects from the API.
+   *
+   * @public
+   * @returns {void}
+   */
   public disconnect = () => {
     this.api.disconnect();
   };
 
+  /**
+   * Creates a new multisig account.
+   *
+   * @public
+   * @param {object} options - The options for creating a multisig account.
+   * @param {string|Uint8Array} [options.metadata] - The metadata for the multisig account (optional).
+   * @param {Perbill|BN|number} options.minimumSupport - The minimum support required for a proposal to pass.
+   * @param {Perbill|BN|number} options.requiredApproval - The required approval threshold for a proposal to pass.
+   * @param {FeeAsset} options.creationFeeAsset - The fee asset to use for creating the multisig account.
+   * @returns {MultisigCreator} - The multisig creator object.
+   */
   public createMultisig = ({
     metadata,
     minimumSupport,
@@ -178,6 +215,13 @@ class Saturn {
     return creator;
   };
 
+  /**
+   * Retrieves the details of a multisig account.
+   *
+   * @public
+   * @param {number} id - The ID of the multisig account.
+   * @returns {Promise<MultisigDetails|null>} - A promise that resolves to the multisig details, or null if the multisig account does not exist.
+   */
   public getDetails = async (id: number): Promise<MultisigDetails | null> => {
     const multisig = (await this._getMultisig(id)).unwrap();
 
@@ -198,31 +242,57 @@ class Saturn {
     return details;
   };
 
+  /**
+   * Retrieves the total token supply in a multisig account.
+   *
+   * @public
+   * @param {number} id - The ID of the multisig account.
+   * @returns {Promise<BN>} - A promise that resolves to the total token supply.
+   */
   public getSupply = async (id: number) => {
     const supply: BN = await this._getTotalIssuance(id);
 
     return supply;
   };
 
+  /**
+   * Retrieves the pending calls in a multisig account.
+   *
+   * @public
+   * @param {number} id - The ID of the multisig account.
+   * @returns {Promise<CallDetailsWithHash[]>} - A promise that resolves to an array of pending call details with hashes.
+   */
   public getPendingCalls = async (
     id: number
   ): Promise<CallDetailsWithHash[]> => {
-      const pendingCalls: [
-          StorageKey<[u32, Hash]>,
-          PalletInv4MultisigMultisigOperation
-      ][] = await this._getPendingMultisigCalls(id);
+    const pendingCalls: [
+      StorageKey<[u32, Hash]>,
+      PalletInv4MultisigMultisigOperation
+    ][] = await this._getPendingMultisigCalls(id);
 
-      const oc = pendingCalls.map(([hash, call]) => {
-          return {
-              callHash: hash.args[1],
-              details: new CallDetails({ id, details: call, registry: this.api.registry, }),
-          };
+    const oc = pendingCalls.map(([hash, call]) => {
+      return {
+        callHash: hash.args[1],
+        details: new CallDetails({
+          id,
+          details: call,
+          registry: this.api.registry,
+        }),
+      };
+    });
 
-      });
-
-      return oc;
+    return oc;
   };
 
+  /**
+   * Retrieves a specific pending call in a multisig account.
+   *
+   * @public
+   * @param {object} options - The options for retrieving a pending call.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {string|Hash} options.callHash - The hash of the call to retrieve.
+   * @returns {Promise<CallDetails|null>} - A promise that resolves to the call details, or null if the call does not exist.
+   */
   public getPendingCall = async ({
     id,
     callHash,
@@ -237,11 +307,22 @@ class Saturn {
 
     if (!call) return null;
 
-      const details = new CallDetails({ id, details: call, registry: this.api.registry, });
+    const details = new CallDetails({
+      id,
+      details: call,
+      registry: this.api.registry,
+    });
 
     return details;
   };
 
+  /**
+   * Retrieves the members of a multisig account.
+   *
+   * @public
+   * @param {number} id - The ID of the multisig account.
+   * @returns {Promise<AccountId[]>} - A promise that resolves to an array of member account IDs.
+   */
   public getMultisigMembers = async (id: number): Promise<AccountId[]> => {
     const keys = await this._getMultisigMembers({ id });
 
@@ -250,6 +331,13 @@ class Saturn {
     return mapped;
   };
 
+  /**
+   * Retrieves the multisig accounts associated with a specific account.
+   *
+   * @public
+   * @param {string|AccountId} account - The account for which to retrieve multisig accounts.
+   * @returns {Promise<{ multisigId: number; tokens: BN }[]>} - A promise that resolves to an array of multisig account IDs and token balances.
+   */
   public getMultisigsForAccount = async (
     account: string | AccountId
   ): Promise<{ multisigId: number; tokens: BN }[]> => {
@@ -271,6 +359,15 @@ class Saturn {
     return mapped;
   };
 
+  /**
+   * Retrieves the token balance of a member in a multisig account.
+   *
+   * @public
+   * @param {object} options - The options for retrieving a member's balance.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {string|AccountId} options.address - The address of the member account.
+   * @returns {Promise<BN>} - A promise that resolves to the member's token balance.
+   */
   public getMultisigMemberBalance = async ({
     id,
     address,
@@ -283,6 +380,17 @@ class Saturn {
     return balance;
   };
 
+  /**
+   * Proposes adding a new member to a multisig account.
+   *
+   * @public
+   * @param {object} options - The options for proposing a new member.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {string|AccountId} options.address - The address of the new member account.
+   * @param {BN} options.amount - The amount of tokens to be transferred to the new member.
+   * @param {string|Uint8Array} [options.proposalMetadata] - The metadata for the proposal (optional).
+   * @returns {MultisigCall} - The multisig call representing the proposal.
+   */
   public proposeNewMember = ({
     id,
     address,
@@ -304,6 +412,17 @@ class Saturn {
     });
   };
 
+  /**
+   * Proposes removing a member from a multisig account.
+   *
+   * @public
+   * @param {object} options - The options for proposing member removal.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {string|AccountId} options.address - The address of the member account to remove.
+   * @param {BN} options.amount - The amount of tokens to be transferred from the removed member to the proposer.
+   * @param {string|Uint8Array} [options.proposalMetadata] - The metadata for the proposal (optional).
+   * @returns {MultisigCall} - The multisig call representing the proposal.
+   */
   public proposeMemberRemoval = ({
     id,
     address,
@@ -325,6 +444,16 @@ class Saturn {
     });
   };
 
+  /**
+   * Votes on a pending multisig call in a multisig account.
+   *
+   * @public
+   * @param {object} options - The options for voting on a pending call.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {string|Hash} options.callHash - The hash of the pending call.
+   * @param {boolean} options.aye - Whether to vote "aye" (true) or "nay" (false).
+   * @returns {void}
+   */
   public vote = ({
     id,
     callHash,
@@ -341,6 +470,15 @@ class Saturn {
     });
   };
 
+  /**
+   * Withdraws a vote on a pending multisig call in a multisig account.
+   *
+   * @public
+   * @param {object} options - The options for withdrawing a vote.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {string|Hash} options.callHash - The hash of the pending call.
+   * @returns {void}
+   */
   public withdrawVote = ({
     id,
     callHash,
@@ -354,6 +492,17 @@ class Saturn {
     });
   };
 
+  /**
+   * Builds a multisig call using a SubmittableExtrinsic, Uint8Array, or Call object.
+   *
+   * @public
+   * @param {object} options - The options for building a multisig call.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {string|Uint8Array} [options.proposalMetadata] - The metadata for the proposal (optional).
+   * @param {FeeAsset} [options.feeAsset] - The fee asset to use for the multisig call (optional).
+   * @param {SubmittableExtrinsic<ApiTypes>|Uint8Array|Call} options.call - The call to include in the multisig.
+   * @returns {MultisigCall} - The multisig call object.
+   */
   public buildMultisigCall = ({
     id,
     proposalMetadata,
@@ -376,6 +525,20 @@ class Saturn {
     );
   };
 
+  /**
+   * Sends an XCM call from a multisig account to a destination.
+   *
+   * @public
+   * @param {object} options - The options for sending an XCM call.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {string} options.destination - The destination of the XCM call.
+   * @param {BN} options.weight - The weight of the XCM call.
+   * @param {`0x{string}`|Uint8Array} options.callData - The call data for the XCM call.
+   * @param {XcmAssetRepresentation} options.xcmFeeAsset - The fee asset for the XCM call.
+   * @param {BN} options.xcmFee - The fee amount for the XCM call.
+   * @param {string|Uint8Array} [options.proposalMetadata] - The metadata for the proposal (optional).
+   * @returns {MultisigCall} - The multisig call representing the XCM call.
+   */
   public sendXCMCall = ({
     id,
     destination,
@@ -404,6 +567,20 @@ class Saturn {
     return this.buildMultisigCall({ id, call, proposalMetadata });
   };
 
+  /**
+   * Transfers an XCM asset from a multisig account to a recipient.
+   *
+   * @public
+   * @param {object} options - The options for transferring an XCM asset.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {XcmAssetRepresentation} options.asset - The XCM asset to transfer.
+   * @param {BN} options.amount - The amount of the XCM asset to transfer.
+   * @param {string|AccountId} options.to - The recipient of the XCM asset.
+   * @param {XcmAssetRepresentation} options.xcmFeeAsset - The fee asset for the XCM transfer.
+   * @param {BN} options.xcmFee - The fee amount for the XCM transfer.
+   * @param {string|Uint8Array} [options.proposalMetadata] - The metadata for the proposal (optional).
+   * @returns {MultisigCall} - The multisig call representing the XCM transfer.
+   */
   public transferXcmAsset = ({
     id,
     asset,
@@ -432,6 +609,20 @@ class Saturn {
     return this.buildMultisigCall({ id, call, proposalMetadata });
   };
 
+  /**
+   * Bridges an XCM asset from a multisig account to a destination chain.
+   *
+   * @public
+   * @param {object} options - The options for bridging an XCM asset.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {XcmAssetRepresentation} options.asset - The XCM asset to bridge.
+   * @param {BN} options.amount - The amount of the XCM asset to bridge.
+   * @param {string} options.destination - The destination chain for the bridged asset.
+   * @param {BN} options.xcmFee - The fee amount for the XCM bridge.
+   * @param {string|AccountId} [options.to] - The recipient of the bridged asset (optional).
+   * @param {string|Uint8Array} [options.proposalMetadata] - The metadata for the proposal (optional).
+   * @returns {MultisigCall} - The multisig call representing the XCM bridge.
+   */
   public bridgeXcmAsset = ({
     id,
     asset,
@@ -460,6 +651,19 @@ class Saturn {
     return this.buildMultisigCall({ id, call, proposalMetadata });
   };
 
+  /**
+   * Sets the parameters of a multisig account.
+   *
+   * @public
+   * @param {object} options - The options for setting multisig parameters.
+   * @param {number} options.id - The ID of the multisig account.
+   * @param {string|Uint8Array} options.proposalMetadata - The metadata for the proposal.
+   * @param {string|Uint8Array} [options.metadata] - The metadata for the multisig account (optional).
+   * @param {Perbill|BN|number} options.minimumSupport - The minimum support required for a proposal to pass.
+   * @param {Perbill|BN|number} options.requiredApproval - The required approval threshold for a proposal to pass.
+   * @param {boolean} options.frozenTokens - Whether the tokens of the multisig account should be frozen.
+   * @returns {MultisigCall} - The multisig call representing the parameter change.
+   */
   public setMultisigParameters = ({
     id,
     proposalMetadata,
@@ -485,6 +689,12 @@ class Saturn {
     return this.buildMultisigCall({ id, call, proposalMetadata });
   };
 
+  /**
+   * Retrieves the status of XCM chains.
+   *
+   * @public
+   * @returns {Promise<{ chainMultilocation: XcmV1MultiLocation; isUnderMaintenance: boolean }[]>} - A promise that resolves to an array of chain status objects.
+   */
   public getXcmStatus = async () => {
     const chains = await this._getChainsUnderMaintenance();
 
